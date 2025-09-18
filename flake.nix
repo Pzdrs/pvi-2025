40fd7e6e7dab8f81f13bc6a82b65e103d598cf43
Petr Boháč <petrbohac3@seznam.cz>
2025-09-19 00:00:48 +0200

    nix flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Dev environment na Chaloupkovo PVI ulohy";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          python311
+          python311Packages.pip
+        ];
+
+        shellHook = ''
+          python -m venv .venv
+          source .venv/bin/activate
+          pip install -r requirements.txt
+        '';
+      };
+    };
+}
